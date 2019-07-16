ad36b3f3313d13b5b071c95002e8441540a4c3aa
samy <samy.t.khelifa@gmail.com>
2019-07-16 23:23:20 +0200

    Screen Rotation and Impact on Views    after       modified:   app/src/main/java/com/example/screenorientation/MainActivity.java       modified:   app/src/main/res/layout/activity_main.xml

--- app/src/main/java/com/example/screenorientation/MainActivity.java
+++ app/src/main/java/com/example/screenorientation/MainActivity.java
@@ -4,9 +4,22 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+
+/**
+ * A Demo app to illustrate :
+ * 1. Activity Lifecycle behaviour on Screen Rotation
+ * 2. Impact of Screen Rotation on Views.
+ */
 
 public class MainActivity extends AppCompatActivity {
     private static final String TAG = MainActivity.class.getSimpleName();
+
+    private Button btn;
+    private TextView textView;
 
 
     @Override
@@ -14,5 +27,51 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         Log.i(TAG, "onCreate()");
+        textView = findViewById(R.id.textView);
+        btn = findViewById(R.id.button);
+
+        btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                btn.setText("LOGOUT");
+                textView.setText("Testing");
+            }
+        });
+    }
+
+    @Override
+    protected void onRestart() {
+        super.onRestart();
+        Log.i(TAG, "onRestart()");
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        Log.i(TAG, "onStart()");
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        Log.i(TAG, "onResume()");
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        Log.i(TAG, "onPause()");
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        Log.i(TAG, "onStop()");
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        Log.i(TAG, "onDestroy()");
     }
 }
